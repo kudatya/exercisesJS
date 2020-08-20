c497daf6eb59803e07221fab0b6a636647b769bc
Карина <tinkOFFSKS4tAsk>
2020-08-20 16:57:22 +0300

    №1 Привет, object

---
+++ ex.js
@@ -0,0 +1,14 @@
+
+
+  function workWithObject() {
+      let user = {
+          name   :  "John",
+          surname:   "Smith"
+      }
+      alert(user.name +"  "+user.surname);
+      user.name = "Peter"
+      alert(user.name);
+      delete user.name;
+      alert(user.surname);
+  }
+  workWithObject();
