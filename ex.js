f92c3b3abf3198c483dbf8cdda17941fc9caaed5
Карина <tinkOFFSKS4tAsk>
2020-08-21 15:25:49 +0300

    ex. 5 Умножаем все числовые свойства на 2

--- ex.js
+++ ex.js
@@ -41,4 +41,22 @@
       }
       alert(sum);
   }
-  sumSalaries(salaries);
+  //sumSalaries(salaries);
+
+  // ex.5 Умножаем все числовые свойства на 2
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  function multiplyNumeric(obj) {
+      for(let key in obj){
+
+          obj[key] *= 2;
+      }
+  }
+  multiplyNumeric(menu);
+
+  alert(menu.title);
+
